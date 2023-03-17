816a46663d99a08d5114f33708562ea37d729fec
MUHAMMAD GILANG <gilang.ariyana@widyatama.ac.id>
2023-03-17 21:36:40 +0700

    Backup Halaman Login

---
+++ resources/views/login.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Heiwa | Login</title>
+    
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500&display=swap" rel="stylesheet">
+    
+    <link rel="stylesheet" href="css/login-design.css">
+</head>
+<body>
+    <div class="container">
+        <div class="login">
+            <form action="">
+                <h1>Welcome</h1>
+                <hr>
+                <p>Please Sign-In to your Account</p>
+                <label for="">Username</label>
+                <input type="text" placeholder="username123">
+                <label for="">Password</label>
+                <input type="password" placeholder="password123">
+                <button>Sign-In</button>
+            </form>
+        </div>
+        <div class="right">
+            <img src="img/login.jpg" alt="">
+        </div>
+    </div>
+</body>
+</html>
