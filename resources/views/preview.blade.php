0a49140bf11aa70bfb25596c75deff1839360223
MUHAMMAD GILANG <gilang.ariyana@widyatama.ac.id>
2023-03-17 22:39:58 +0700

    Backup Halaman Preview(Belum termasuk Nav dan Footer)

---
+++ resources/views/preview.blade.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Heiwa | Articles</title>
+ 
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500&display=swap" rel="stylesheet">
+ 
+    <link rel="stylesheet" href="css/preview-design.css">
+</head>
+<body>
+    <div class="container">
+        <div class="article">
+            
+            <div class="article-header">
+                <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, minus! </h1>
+                <span class="tag tag-blue">Tag</span>
+            </div>
+            
+            <h5>By Admin - DD/MM/YY</h5>
+            <div class="article-header-img"><img src="img/4.jpg" alt=""></div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vestibulum lectus mauris ultrices eros in cursus turpis massa. Odio ut sem nulla pharetra diam. Nisi porta lorem mollis aliquam ut porttitor. Enim sit amet venenatis urna cursus eget nunc scelerisque viverra. Tortor aliquam nulla facilisi cras fermentum. Enim ut sem viverra aliquet eget sit amet. Diam sit amet nisl suscipit adipiscing bibendum est. Amet luctus venenatis lectus magna fringilla urna porttitor rhoncus dolor. Diam sollicitudin tempor id eu nisl nunc. Erat imperdiet sed euismod nisi porta lorem. Lectus proin nibh nisl condimentum id venenatis. Et netus et malesuada fames ac turpis egestas sed. Phasellus vestibulum lorem sed risus ultricies tristique nulla. Suspendisse sed nisi lacus sed viverra tellus in. Vestibulum rhoncus est pellentesque elit ullamcorper. Aliquet bibendum enim facilisis gravida neque convallis. Vitae justo eget magna fermentum iaculis.</p>
+            <p>Ac ut consequat semper viverra nam libero. Urna nec tincidunt praesent semper feugiat nibh. Sed augue lacus viverra vitae congue eu. Porttitor massa id neque aliquam vestibulum. Vel pretium lectus quam id leo in vitae turpis. Tristique risus nec feugiat in fermentum posuere urna. Dictumst quisque sagittis purus sit amet volutpat. Scelerisque eleifend donec pretium vulputate sapien nec sagittis. Adipiscing elit duis tristique sollicitudin nibh sit amet. Nulla pharetra diam sit amet nisl suscipit adipiscing bibendum. Euismod in pellentesque massa placerat duis ultricies. Interdum velit euismod in pellentesque massa. Risus feugiat in ante metus dictum at tempor. Id consectetur purus ut faucibus. Diam phasellus vestibulum lorem sed risus ultricies tristique nulla. Commodo odio aenean sed adipiscing diam donec adipiscing. Vel facilisis volutpat est velit egestas dui id ornare arcu.</p>
+            <div class="wraper">
+                <div class="article-img1"><img src="img/4.jpg" alt=""></div>
+                <div class="article-img2"><img src="img/4.jpg" alt=""></div>
+            </div>
+            <p>Aliquet porttitor lacus luctus accumsan tortor posuere ac. Vel elit scelerisque mauris pellentesque pulvinar. Tristique nulla aliquet enim tortor at auctor urna. Condimentum mattis pellentesque id nibh tortor id. Habitant morbi tristique senectus et netus et malesuada fames. Eget est lorem ipsum dolor sit amet. Sed egestas egestas fringilla phasellus faucibus. Urna cursus eget nunc scelerisque viverra mauris in aliquam sem. Molestie a iaculis at erat pellentesque adipiscing commodo elit at. Viverra accumsan in nisl nisi. Cursus mattis molestie a iaculis at erat pellentesque adipiscing commodo. Gravida quis blandit turpis cursus. Quam nulla porttitor massa id. Tellus in metus vulputate eu scelerisque felis imperdiet. Nulla facilisi nullam vehicula ipsum. Fames ac turpis egestas sed. Porttitor lacus luctus accumsan tortor posuere ac ut consequat.
+            Sapien faucibus et molestie ac feugiat sed lectus. Turpis cursus in hac habitasse platea dictumst quisque sagittis purus. Fringilla phasellus faucibus scelerisque eleifend donec pretium vulputate sapien. Tempus urna et pharetra pharetra massa massa ultricies. Curabitur gravida arcu ac tortor dignissim convallis aenean et tortor. Tellus molestie nunc non blandit. Sit amet dictum sit amet justo donec enim diam. Mattis ullamcorper velit sed ullamcorper morbi tincidunt. Maecenas accumsan lacus vel facilisis volutpat. Enim praesent elementum facilisis leo vel. Pulvinar pellentesque habitant morbi tristique senectus et netus et malesuada. Ullamcorper a lacus vestibulum sed. Rutrum tellus pellentesque eu tincidunt tortor aliquam nulla facilisi. Vitae proin sagittis nisl rhoncus mattis rhoncus.
+            Vitae proin sagittis nisl rhoncus mattis rhoncus urna neque. Pulvinar etiam non quam lacus suspendisse faucibus interdum posuere. Nisl vel pretium lectus quam id. Maecenas pharetra convallis posuere morbi leo. Porta nibh venenatis cras sed. Purus viverra accumsan in nisl. Dui id ornare arcu odio ut sem nulla. Pellentesque habitant morbi tristique senectus et netus et. Vulputate sapien nec sagittis aliquam malesuada bibendum arcu vitae elementum. Nibh ipsum consequat nisl vel pretium lectus quam.</p>
+ 
+            <div class="wraper2">
+                <div class="article-img3"><img src="img/4.jpg" alt=""></div>
+                <div class="article-img4"><img src="img/4.jpg" alt=""></div>
+            </div>
+            
+            <hr/>
+ 
+            <div class="article-footer">
+                <h1>Other Articles</h1>
+                <!-- <a href="/">Read More &#8594;</a> -->
+            </div>
+ 
+            <div class="card-wraper">
+                    <div class="card">
+                        <div class="card-image">
+                            <img src="img/2.jpg" alt="" />
+                        </div>
+                        <div class="card-info">
+                            <span>Tag</span>
+                            <div class="card-title">
+                                <a href="/"
+                                    >Lorem ipsum dolor sit amet, consectetur
+                                    adipiscing elit, sed do eiusmod.</a
+                                >
+                            </div>
+                            <div class="card-footer">
+                                <a href="/">Read More &#8594;</a>
+                                <small>DD/MM/YY</small>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="card">
+                        <div class="card-image">
+                            <img src="img/1.jpg" alt="" />
+                        </div>
+                        <div class="card-info">
+                            <span>Tag</span>
+                            <div class="card-title">
+                                <a href="/"
+                                    >Lorem ipsum dolor sit amet, consectetur
+                                    adipiscing elit, sed do eiusmod.</a
+                                >
+                            </div>
+                            <div class="card-footer">
+                                <a href="">Read More &#8594;</a>
+                                <small>DD/MM/YY</small>
+                            </div>
+                        </div>
+                    </div>
+            </div>
+        </div>
+        
+        
+        <div class="sidebar">
+            <p>Read Also: </p>
+                    <div class="card">
+                        <div class="card-image">
+                            <img src="img/1.jpg" alt="" />
+                        </div>
+                        <div class="card-info">
+                            <div class="card-title">
+                                <a href="/"
+                                    >Lorem ipsum dolor sit amet, consectetur
+                                    adipiscing elit, sed do eiusmod.</a
+                                >
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="card">
+                        <div class="card-image">
+                            <img src="img/2.jpg" alt="" />
+                        </div>
+                        <div class="card-info">
+                            <div class="card-title">
+                                <a href="/"
+                                    >Lorem ipsum dolor sit amet, consectetur
+                                    adipiscing elit, sed do eiusmod.</a
+                                >
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="card">
+                        <div class="card-image">
+                            <img src="img/1.jpg" alt="" />
+                        </div>
+                        <div class="card-info">
+                            <div class="card-title">
+                                <a href="/"
+                                    >Lorem ipsum dolor sit amet, consectetur
+                                    adipiscing elit, sed do eiusmod.</a
+                                >
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="card">
+                        <div class="card-image">
+                            <img src="img/2.jpg" alt="" />
+                        </div>
+                        <div class="card-info">
+                            <div class="card-title">
+                                <a href="/"
+                                    >Lorem ipsum dolor sit amet, consectetur
+                                    adipiscing elit, sed do eiusmod.</a
+                                >
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="card">
+                        <div class="card-image">
+                            <img src="img/1.jpg" alt="" />
+                        </div>
+                        <div class="card-info">
+                            <div class="card-title">
+                                <a href="/"
+                                    >Lorem ipsum dolor sit amet, consectetur
+                                    adipiscing elit, sed do eiusmod.</a
+                                >
+                            </div>
+                        </div>
+                    </div>
+        </div>
+    </div>
+    
+    <div class="footer">
+        <h2>Footer</h2>
+    </div>
+    
+</body>
+</html>
