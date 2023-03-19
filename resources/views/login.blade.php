61ed1d6ae5e31fd0c296f95b7c516ac6eebc5ff3
zaldyalfarez <zaldyalfarez@gmail.com>
2023-03-19 12:18:54 +0700

    halaman login kelar

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,28 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Heiwa | Login</title>
-    
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500&display=swap" rel="stylesheet">
-    
-    <link rel="stylesheet" href="css/login-design.css">
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-    <!-- Box Icons -->
-    <link
+        <!-- Link CSS -->
+        <link rel="stylesheet" href="css/login-design.css" />
+
+        <!-- Google Fonts -->
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+            href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500&display=swap"
+            rel="stylesheet"
+        />
+
+        <!-- Box Icons -->
+        <link
             href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css"
             rel="stylesheet"
         />
 
-</head>
-<body>
-
-    <!-- Navigation Bar -->
-    <nav>
+        <title>Heiwa</title>
+    </head>
+    <body>
+        <!-- Navigation Bar -->
+        <nav>
             <div class="navbar">
                 <i class="bx bx-menu side-open"></i>
                 <span class="logo navLogo">
@@ -61,30 +65,49 @@
         </nav>
         <!-- End of Navigation Bar -->
 
-    <!-- Login Page -->
-    <div class="container">
-        <div class="wraper">
+        <!-- Login -->
+        <div class="main-container">
             <div class="login">
-                <form action="">
-                    <h1>Welcome</h1>
-                    <hr>
-                    <p>Please Sign-In to your Account</p>
-                    <label for="">Username</label>
-                    <input type="text" placeholder="username123">
-                    <label for="">Password</label>
-                    <input type="password" placeholder="password123">
-                    <button>Sign-In</button>
-                </form>
-            </div>
-            <div class="right">
-                <img src="img/login.jpg" alt="">
+                <div class="login-form-wrapper">
+                    <div class="login-form">
+                        <h1>WELCOME</h1>
+                        <small>Please Sign In to your Account</small>
+
+                        <form>
+                            <label class="label" for="username">Username</label>
+                            <input
+                                class="box"
+                                type="username"
+                                id="username"
+                                name="username"
+                                placeholder="Enter your username"
+                                required
+                            />
+
+                            <label class="label" for="password">Password</label>
+                            <input
+                                class="box"
+                                type="password"
+                                id="password"
+                                name="password"
+                                placeholder="Enter your password"
+                                required
+                            />
+
+                            <button class="login-button" type="submit">
+                                SIGN IN
+                            </button>
+                        </form>
+                    </div>
+                </div>
+                <div class="login-image">
+                    <img src="img/login-image.jpg" alt="" />
+                </div>
             </div>
         </div>
-    </div>
-    
-    <!-- End of Login Page -->
+        <!-- End of Login -->
 
-    <!-- Footer -->
+        <!-- Footer -->
         <footer>
             <div class="footer-container">
                 <div class="footer-info">
@@ -94,7 +117,7 @@
             </div>
         </footer>
         <!-- End of Footer -->
-    
-    <script src="js/login-script.js"></script>
-</body>
+
+        <script src="js/login-script.js"></script>
+    </body>
 </html>
