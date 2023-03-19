ff671439b1782f09184b5853e174221425c85842
zaldyalfarez <zaldyalfarez@gmail.com>
2023-03-19 21:09:47 +0700

    halaman preview kelar, tinggal minor update

---
+++ public/js/preview-script.js
@@ -0,0 +1,26 @@
+// Navigation Bar
+const nav = document.querySelector("nav");
+const body = document.querySelector("body");
+const searchToggle = document.querySelector(".searchToggle");
+const sidebarC = document.querySelector(".side-close");
+const sidebarO = document.querySelector(".side-open");
+
+searchToggle.addEventListener("click", () => {
+    searchToggle.classList.toggle("active");
+});
+
+sidebarO.addEventListener("click", () => {
+    nav.classList.add("active");
+});
+
+body.addEventListener("click", (e) => {
+    let clicked = e.target;
+
+    if (
+        !clicked.classList.contains("side-open") &&
+        !clicked.classList.contains("menu")
+    ) {
+        nav.classList.remove("active");
+    }
+});
+// End of Navigation Bar
--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('/preview', function () {
     return view('preview');
 });
