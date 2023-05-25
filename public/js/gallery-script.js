e38bc0c6ae8e922f53a2738610648911f0c80a00
asenatribanyu <asenatribanyu@gmail.com>
2023-05-25 22:58:50 +0700

    galery js visit and delete

--- public/js/gallery-script.js
+++ public/js/gallery-script.js
@@ -14,9 +14,24 @@ imagesLoaded(grid, function () {
 //Image Popup
 const imgList = document.querySelectorAll(".img-list");
 const imgPopup = document.querySelector(".img-popup");
+const popupImage = imgPopup.querySelector("img");
+const popupCaption = imgPopup.querySelector(".popup-caption p");
+const visitButton = imgPopup.querySelector(".visit");
+const deleteButton = imgPopup.querySelector(".delete");
 
 imgList.forEach((img) => {
     img.addEventListener("click", () => {
+        // Get the necessary data from the clicked grid item
+        const src = img.getAttribute("src");
+        const caption = img.getAttribute("data-caption");
+        const slug = img.getAttribute("data-slug");
+        const deleted = img.getAttribute("data-delete");
+
+        // Update the image source, caption, and slug in the popup
+        popupImage.src = src;
+        popupCaption.textContent = caption;
+        visitButton.href = `/categories${slug}`;
+
         imgPopup.classList.add("active");
     });
 });
